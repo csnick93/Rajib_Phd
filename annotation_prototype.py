3cfbe6d096ad74b8d53df83c65c29bc3f89e6400
Nick <nickvonroden@gmail.com>
2019-11-24 17:00:28 +0100

    iterate through multiple images

--- annotation_prototype.py
+++ annotation_prototype.py
@@ -1,4 +1,5 @@
 import cv2
+from PIL import Image
 from tqdm import tqdm
 import os
 import numpy as np
@@ -9,10 +10,11 @@ finished_polygons = []
 
 
 def draw_polygon(event, x, y, flags, param):
-    global ix, iy
+    global img
     if event == cv2.EVENT_LBUTTONDOWN:
         polygon_in_drawing.append((x, y))
     elif event == cv2.EVENT_LBUTTONUP:
+        import pdb; pdb.set_trace()
         if len(polygon_in_drawing) > 1:
             cv2.line(img, polygon_in_drawing[-2], polygon_in_drawing[-1],
                      (0, 255, 0), 2)
@@ -43,13 +45,16 @@ def polygons_as_segmask():
     return seg_mask
 
 
-#TODO: load image to be annotated
-img = np.zeros((512, 512, 3), np.uint8)
+save_folder = '/home/nick/segmentations'
+input_img_folder = '/home/nick/input_imgs'
+input_imgs = os.listdir(input_img_folder)
 cv2.namedWindow('image')
 cv2.setMouseCallback('image', draw_polygon)
-save_folder = '/home/nick/segmentations'
+im_ctr = 0
+im_file = os.path.join(input_img_folder, input_imgs[im_ctr])
+img = np.asarray(Image.open(im_file))[:,:,::-1]
 
-while (1):
+while (1): 
     cv2.imshow('image', img)
     k = cv2.waitKey(1) & 0xFF
     if k == ord('q'):
@@ -65,7 +70,13 @@ while (1):
     elif k == ord('s'):
         # save polygons as segmentation map
         segmask = polygons_as_segmask()
-        filename = os.path.join(save_folder, 'seg_mask.npy')
+        filename = os.path.join(save_folder, 'seg_mask_%i.npy'%im_ctr)
         np.save(filename, segmask)
-
+    elif k == ord('n'):
+        if im_ctr == len(input_imgs)-1:
+            im_ctr = 0
+        else:
+            im_ctr += 1
+        im_file = os.path.join(input_img_folder, input_imgs[im_ctr])
+        img = np.asarray(Image.open(im_file))[:,:,::-1]
 cv2.destroyAllWindows()
